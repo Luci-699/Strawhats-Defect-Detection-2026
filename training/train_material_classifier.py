caac28d05a1553001cb86b196cfc2d18190152fb
Faizz <madmaxboi.98@gmail.com>
2026-07-31 12:20:19 +0530

    fix: add transforms.Resize((224, 224)) to material classifier dataloader

--- training/train_material_classifier.py
+++ training/train_material_classifier.py
@@ -16,6 +16,7 @@ def train_model(args):
     
     data_transforms = {
         'train': transforms.Compose([
+            transforms.Resize((224, 224)),
             transforms.RandomHorizontalFlip(),
             transforms.RandomRotation(15),
             transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
@@ -23,10 +24,12 @@ def train_model(args):
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
         'val': transforms.Compose([
+            transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ]),
         'test': transforms.Compose([
+            transforms.Resize((224, 224)),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
         ])
